719bdd8131533a62e4a7faa520463bb41e15bc1c
vladalbescu <66447648+vladalbescu@users.noreply.github.com>
2020-09-02 14:27:28 +0300

    Updated design

--- nan-dashboard/app/views/partials/nav.php
+++ nan-dashboard/app/views/partials/nav.php
@@ -1,19 +1,19 @@
-<nav class="navbar border-bottom">
-<div class="container">
-  <a class="navbar-brand mr-auto" href="/">NaN Team</a>
+<nav class="navbar border-bottom bg-white">
+  <div class="container">
+    <a class="navbar-brand mr-auto" href="/">NaN Team</a>
     <ul class="nav">
-    <?php if(isset($_SESSION['user'])) : ?>
-      <li class="nav-item">
-        <a class="nav-link <?php if(Request::uri() == "") echo "active" ?>" href="/">Acasă</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link <?php if(Request::uri() == "dashboard") echo "active" ?>" href="dashboard">Dashboard</a>
-      </li>
-    <?php endif; ?>
+      <?php if (isset($_SESSION['user'])) : ?>
+        <li class="nav-item">
+          <a class="nav-link <?php if (Request::uri() == "") echo "active" ?>" href="/">Acasă</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?php if (Request::uri() == "dashboard") echo "active" ?>" href="dashboard">Dashboard</a>
+        </li>
+      <?php endif; ?>
     </ul>
-    <?php if(isset($_SESSION['user'])): ?>
+    <?php if (isset($_SESSION['user'])) : ?>
       <a class="btn btn-primary" href="logout" role="button">Logout</a>
     <?php endif; ?>
-</div>
+  </div>
 
 </nav>
